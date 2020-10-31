35e504eb5f4550b0c42b916c3ad803d5b99dada0
Conner Luzier <connerluzier@protonmail.com>
2020-10-31 14:20:21 -0400

    scraping working, removed commands

--- commands/scrape.js
+++ commands/scrape.js
@@ -1,21 +1,28 @@
 module.exports = {
-  name: "uoa",
+  name: "scrape",
   description: "Shows user unusual options activity",
-  execute(message) {
-    const fs = require("fs");
+  async execute(message) {
+    const request = require("request-promise");
     const cheerio = require("cheerio");
-    const got = require("got");
+    const Url = "https://www.marketbeat.com/market-data/unusual-call-options-volume/";
 
-    const vgmUrl = "https://www.barchart.com/options/unusual-activity/stocks";
-
-    got(vgmUrl)
-      .then((response) => {
-        const $ = cheerio.load(response.body);
-        console.log($("span"));
-        message.author.send($("span"));
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    const result = await request.get({
+      uri: Url,
+      headers: {
+          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+          'Accept-Encoding': 'gzip, deflate, br',
+          'Cache-Control': 'max-age=0',
+          'Connection': 'keep-alive',
+          'Host': 'www.marketbeat.com',
+          'Upgrade-Insecure-Requests': '1',
+          'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36'
+      },
+      gzip: true
+  });
+    const $ = cheerio.load(result);
+    $('#cphPrimaryContent_pnlUpdate > div.scroll-table-wrapper-wrapper > div > table > tbody > tr > td > a > div.ticker-area').each((index, element) => {
+      console.log($(element).text());
+      message.channel.send($(element).text());
+    });
   },
 };
